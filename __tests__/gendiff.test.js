cbde4b0eafbcd5a050cf7b560c667ddc2abfcafa
Konstantin Shagan <konstantinshagan@gmail.com>
2024-09-05 20:57:33 +0300

    added two yml files into fixtures, made test for yml files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,15 +8,28 @@ import genDiff from '../src/genDiff.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-test('genDiff', () => {
+test('genDiff for json', () => {
   const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
   const readFixtureFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
   const file1Path = getFixturePath('file1.json');
   const file2Path = getFixturePath('file2.json');
 
-  const fn = genDiff(file1Path, file2Path);
+  const input = genDiff(file1Path, file2Path);
   const expected = readFixtureFile('expected_file.txt');
 
-  expect(fn).toEqual(expected);
+  expect(input).toEqual(expected);
+});
+
+test('genDiff for yml', () => {
+  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+  const readFixtureFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+  const file1Path = getFixturePath('file1.yml');
+  const file2Path = getFixturePath('file2.yml');
+
+  const input = genDiff(file1Path, file2Path);
+  const expected = readFixtureFile('expected_file.txt');
+
+  expect(input).toEqual(expected);
 });
